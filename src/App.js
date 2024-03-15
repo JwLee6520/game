fe94233855bbbd162abe34b35b2a8c140d62b92f
jungwoo <dlwjddn6520@gmail.com>
2024-03-15 21:14:49 +0900

    plus img

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const scis = Math.random();
+
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className='scissors'><img src="img/images.png" alt="img" /></div>
+        <div className='rock'><img src="img/rock.jpeg" alt="img" /></div>
+        <div className='paper'><img src="img/paper.png" alt="img" /></div>
+      </div>
     </div>
   );
 }
