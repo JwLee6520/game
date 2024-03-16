15be338c17e197031118e174bad6b4fc99ce0b7e
jungwoo <dlwjddn6520@gmail.com>
2024-03-16 23:44:03 +0900

    random

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-const scis = Math.random();
-
+const x = Math.flooer(Math.random() *3 + 1);
+console.log(x);
   return (
     <div className="App">
+      <u>가위바위보 시작!</u>
       <div className="container">
-        <div className='scissors'><img src="img/images.png" alt="img" /></div>
-        <div className='rock'><img src="img/rock.jpeg" alt="img" /></div>
-        <div className='paper'><img src="img/paper.png" alt="img" /></div>
+        <div className='scissors'><img src="img/images.png" alt="img" /><p>가위</p></div>
+        <div className='rock'><img src="img/rock.jpeg" alt="img" /><p>바위</p></div>
+        <div className='paper'><img src="img/paper.png" alt="img" /><p>보</p></div>
       </div>
+
     </div>
   );
 }
